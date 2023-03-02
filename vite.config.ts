0707e92ce14b995fd5fd4d68ad54ed437aa17f88
Gleb <gleblubiskin@gmail.com>
2023-03-02 21:48:10 +0300

    develop - Настроил аллиасы и vite.config.ts для создание библиотеки

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
+import dtsPlugin from "vite-plugin-dts"
+
+const pathResolve = (src: string) => path.resolve(__dirname, src);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dtsPlugin({
+    insertTypesEntry: true,
+  })],
+  resolve: {
+    alias: {
+      "@assets": pathResolve("./src/assets"),
+      "@styles": pathResolve("./src/assets/styles"),
+    }
+  },
+  build: {
+    lib: {
+      // путь к основному файлу библиотеки
+      entry: pathResolve( "src/lib/index.ts"),
+      // название библиотеки
+      name: "ReactTSLib",
+      // форматы генерируемых файлов
+      formats: ["es", "umd"],
+      // названия генерируемых файлов
+      fileName: (format) => `react-ts-lib.${format}.js`,
+    },
+    // https://vitejs.dev/config/build-options.html#build-rollupoptions
+    rollupOptions: {
+      external: ["react", "react-dom", "styled-components"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "styled-components": "styled",
+        },
+      },
+    }
+  }
 })
