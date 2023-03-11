95f10e6b2722dba0811dfff2d843ae15464fdfff
LumbagoG <gleblubiskin@gmail.com>
2023-03-11 17:22:52 +0300

    t1 - Кастомизация storybook

--- .storybook/myTheme.ts
+++ .storybook/myTheme.ts
@@ -1,6 +1,39 @@
+// Библиотека
 import { create } from "@storybook/theming";
 
+/**
+ * Моя тема storybook
+ */
 export const myTheme = create({
     base: "dark",
-    brandTitle: "My custom storybook",
+
+    // UI
+    appBg: "#0d1117",
+    appContentBg: "#0d1117",
+    appBorderColor: "#c9d1d9",
+    appBorderRadius: 4,
+
+    // Typography
+    fontBase: '"Open Sans", sans-serif',
+    fontCode: "monospace",
+
+    // Text colors
+    textColor: "#c9d1d9",
+    textInverseColor: "#c9d1d9",
+
+    // Toolbar default and active colors
+    barTextColor: "#c9d1d9",
+    barSelectedColor: "#c9d1d9",
+    barBg: "#0d1117",
+
+    // Form colors
+    inputBg: "#c9d1d9",
+    inputBorder: "#c9d1d9",
+    inputTextColor: "#c9d1d9",
+    inputBorderRadius: 4,
+
+    brandTitle: "LumbagoG",
+    brandUrl: "https://github.com/LumbagoG",
+    brandImage: "https://avatars.githubusercontent.com/u/47673320?v=4",
+    brandTarget: "_blank",
 });
