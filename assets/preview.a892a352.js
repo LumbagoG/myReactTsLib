1963bc232ef87d16ada03721306af88307fdd1c8
LumbagoG <LumbagoG@users.noreply.github.com>
2023-03-26 18:45:53 +0000

    Deploying to gh-pages from @ LumbagoG/myReactTsLib@fb3110fcb622029f983563e12fad9d1ebdc0c790 🚀

---
+++ assets/preview.a892a352.js
@@ -0,0 +1,19 @@
+var a=Object.defineProperty;var e=(o,r)=>a(o,"name",{value:r,configurable:!0});import{W as s,X as n,F as c}from"./styled-components.browser.esm.645203c9.js";import{a as i,j as t}from"./jsx-runtime.87efd77c.js";import"./iframe.5a0e119c.js";const l=s`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: 0;
+    }
+
+    html {
+        font-family: "Inter", sans-serif;
+    }
+
+    body {
+        width: 100%;
+        height: 100%;
+        background-color: aliceblue;
+    }
+`,m=n(l),d=e(o=>i(c,{theme:{},children:[t(m,{}),t(o,{})]}),"WithThemeDecorator"),u={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},layout:"centered"},x=[d];export{x as decorators,u as parameters};
+//# sourceMappingURL=preview.a892a352.js.map
